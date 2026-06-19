53b548a99f6f2c4019ec713502ae5179ef757629
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-06-19 01:09:21 -0400

    Update excel.py

--- app/excel.py
+++ app/excel.py
@@ -1,35 +1,7 @@
-SELECT
-    'PA Enrollment' AS Status_Type,
-    PA_enrollment_status_description AS Status,
-    COUNT(*) AS Total
+SELECT *
 FROM dbo.DuplicateEnrollment_Overlap
-GROUP BY PA_enrollment_status_description
-
-UNION ALL
-
-SELECT
-    'PA Enrollee',
-    PA_enrollee_status_description,
-    COUNT(*)
-FROM dbo.DuplicateEnrollment_Overlap
-GROUP BY PA_enrollee_status_description
-
-UNION ALL
-
-SELECT
-    'Subscriber Enrollment',
-    S_enrollment_status_description,
-    COUNT(*)
-FROM dbo.DuplicateEnrollment_Overlap
-GROUP BY S_enrollment_status_description
-
-UNION ALL
-
-SELECT
-    'Subscriber Enrollee',
-    S_enrollee_status_description,
-    COUNT(*)
-FROM dbo.DuplicateEnrollment_Overlap
-GROUP BY S_enrollee_status_description
-
-ORDER BY Status_Type, Status;
+WHERE
+    PA_enrollment_status_description='Cancelled'
+ OR PA_enrollee_status_description='Cancelled'
+ OR S_enrollment_status_description='Cancelled'
+ OR S_enrollee_status_description='Cancelled';
