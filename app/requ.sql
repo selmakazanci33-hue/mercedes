12db2a99cec3591490d3dba67edd168a4c0b92eb
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-07-11 23:32:02 -0400

    Create requ.sql

---
+++ app/requ.sql
@@ -0,0 +1,2 @@
+This email to Hari is great and I agree with what you said. In addition to this email, we can also send the file to Hari which shows enrollee count for each issuer based on their status. Below is the template. This is what hari was mentioning on Friday 
+If we can fill in the data in these we can give it to hari and he can reconcile. 
