46607a42e45b860d7484c227a96f8e5af7ba1612
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-07-14 14:00:21 -0400

    Create errorslogs.sql

---
+++ app/errorslogs.sql
@@ -0,0 +1,13 @@
+Incorrect syntax near 'b'.  Expecting '(', or SELECT.
+Incorrect syntax near ','.
+Incorrect syntax near ')'.
+Incorrect syntax near 'ORDER'.  Expecting '(', or SELECT.
+Incorrect syntax near 'DISTINCT'.  Expecting '(', or SELECT.
+Incorrect syntax near '*'.  Expecting '(', or SELECT.
+Incorrect syntax near 'DISTINCT'.  Expecting '(', or SELECT.
+Incorrect syntax near 'DISTINCT'.  Expecting '(', or SELECT.
+Incorrect syntax near 'b'.  Expecting '(', or SELECT.
+Incorrect syntax near 'DISTINCT'.  Expecting '(', or SELECT.
+Incorrect syntax near 'ORDER'.  Expecting '(', or SELECT.
+Incorrect syntax near 'DISTINCT'.  Expecting '(', or SELECT.
+Incorrect syntax near 'b'.  Expecting '(', or SELECT.
