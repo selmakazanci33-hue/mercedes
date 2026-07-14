cc36459726c34402ef2059f98f8ce691e3390e90
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-07-13 22:19:34 -0400

    Create swathi.sql

---
+++ app/swathi.sql
@@ -0,0 +1,91 @@
+Hari,
+
+ 
+
+The analysis we completed today focused on two key areas:
+
+ 
+
+Verifying whether each Policy ID listed under Enrollments has a corresponding entry in the UI under the Account Activity section.
+ 
+
+Reviewing five sample HH IDs to confirm whether it was expected behavior for certain 834s to not include enrollee information.
+ 
+
+Here are our findings for the first question :
+
+ 
+
+To verify this, we downloaded the CAP history from UI and filtered on Inbound 834 entries to reconcile based on Policy ID. We observed that Policy IDs with coverage dates from January 1, 2025 through December 31, 2025 do not have an entry for an inbound 834 under the Account Activity section. We must investigate further to confirm if this is expected behavior.
+
+ 
+
+Below are couple of HH IDs that I sampled that did not have an Inbound 834 entry
+
+Tiansia Mitchell (ID: 193630) and Amy Paquet (ID: 771163)
+
+ 
+
+There was one example listed below where there is one entry seen for Inbound 834 with Policy ID 129322. Policy is for PY 2025. However, the label reads “ 834 Inbound Data Rejected” . Confirmation date displayed is 10/23/2024. We need to find a way to investigate why the 834 was rejected. Was it due to /in/bad or /in/duplicate or /in/unknown. Once we get the reason for rejection, we can determine if a resubmission is needed.  
+
+ 
+
+Regarding the second question :
+
+ 
+
+Our spot check on the sample HH IDs showed that some enrollments had only a single member, which aligned with the expected outcome. However, for households with multiple enrollees, we also saw cases where enrollee information was not present in the 834. This behavior appears unexpected and could indicate either subscriber‑only coverage or an upstream issue where dependent enrollee data was not generated. There could be few other reasons, but we can send these 176 cases to Vimo to see if they will have a way to tell us how and why these 834s were missing enrollee info. This will need further review to confirm the expected behavior.
+
+ 
+
+Below are the HH IDs we sampled
+
+ 
+
+
+
+HH ID
+
+Conclusions
+
+ 
+
+Toby Ellis (ID: 217395)
+
+ 
+
+Enrollees are missing as we have a CANCEL enrollment for 2026. There are no other active policies. We should technically see 4 enrollees as a part of these CANCEL enrollments
+
+ 
+
+MA GARCIA (ID: 1043903)
+
+ 
+
+Enrollees should not be missing as there are 2 enrollees as a part of this Policy.
+
+ 
+
+Tiansia Mitchell (ID: 193630)
+
+ 
+
+Enrollees should not be missing as there are 2 enrollees as a part of this Policy.
+
+ 
+
+We wanted to share these observations to confirm whether our analysis is on the right track or if any course correction is needed
+
+ 
+
+Swathi Raghuram Bharatwaj
+
+User Acceptance Test Lead
+
+Office of Commissioner John F. King
+
+Georgia Access
+
+Two Martin Luther King Jr Drive SE
+
+Suite 702 West Tower
